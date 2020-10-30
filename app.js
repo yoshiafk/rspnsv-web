4f274e3678392e8d914c181f0292dd4a8823be9d
Yosy AF <shayr210@gmail.com>
2020-10-30 17:41:58 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+const chk = document.getElementById("chk");
+
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
